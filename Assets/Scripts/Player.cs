373df7a06219e5835b1307b5ed80cd6caadcb54b
KaynirGames <kaynir.games@gmail.com>
2020-05-26 22:42:01 +0300

    Реализовал перемещение игрока

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController playerController;
+    public float moveSpeed = 30f; // Скорость перемещения игрока.
+
+    private float moveX; // Перемещение по оси X.
+    private float moveY; // Перемещение по оси Y.
+
+    private void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        moveY = Input.GetAxis("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        playerController.Move(moveX * Time.fixedDeltaTime, moveY * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, Range(0f, 0.3f)] private float moveSmoothing = 0.05f; // Величина сглаживания перемещения.
+
+    private Rigidbody2D rb;
+
+    private Vector3 currentVelocity = Vector3.zero; // Модифицируется функцией сглаживания.
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Перемещение персонажа в заданном направлении.
+    /// </summary>
+    /// <param name="moveX">Перемещение по оси X.</param>
+    /// <param name="moveY">Перемещение по оси Y.</param>
+    public void Move(float moveX, float moveY)
+    {
+        Vector3 targetVelocity = new Vector3(moveX * 10f, moveY * 10f);
+
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref currentVelocity, moveSmoothing);
+    }
+}
